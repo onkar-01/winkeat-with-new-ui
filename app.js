91287cfb2903dd59afe89a883efa2a365e503ed5
Onkar Vatsa <onkarvatsa@gmail.com>
2023-09-24 13:38:02 +0530

    add cors url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 // );
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://winkeat-with-new-ui-frontend.vercel.app/",
   credentials: true, // Allow credentials (cookies)
 };
 
